d81fa968f79fb41852406eae3005a8a3b04b9b50
Tommy Marshall <tommy.marshall@viget.com>
2016-06-24 01:52:43 -0400

    Adds GA tracking

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -37,6 +37,15 @@
   <link href='https://fonts.googleapis.com/css?family=Tangerine:400,700|Noto+Sans:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
 </head>
 <body>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-79857827-1', 'auto');
+    ga('send', 'pageview');
+  </script>
+
   @yield('content')
 </body>
 </html>
