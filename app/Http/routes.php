4710edd3711fb7f16f9355c9be8b3c515852e4d4
Tommy Marshall <tmarshall@medallia.com>
2017-05-13 10:24:19 -0400

    remove rsvp route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -8,17 +8,17 @@ Route::get('success', function () {
     return view('success');
 });
 
-Route::post('rsvp', function (Illuminate\Http\Request $request) {
-    $answers = collect($request->all())->reject(function($input, $key) {
-        return in_array($key, ['_method', '_token']);
-    });
+// Route::post('rsvp', function (Illuminate\Http\Request $request) {
+//     $answers = collect($request->all())->reject(function($input, $key) {
+//         return in_array($key, ['_method', '_token']);
+//     });
 
-    \Mail::send('emails.rsvp', ['request' => $answers->all()], function ($m) use ($answers) {
-        $m->from('postmaster@charlotteandtommy.com', 'CharlotteAndTommy.com');
-        $m->to('tommyjmarshall@gmail.com', 'Tommy Marshall');
-        $m->cc('cpince88@gmail.com', 'Charlotte Pince');
-        $m->subject($answers->get('name') . ' has RSVPed!');
-    });
+//     \Mail::send('emails.rsvp', ['request' => $answers->all()], function ($m) use ($answers) {
+//         $m->from('postmaster@charlotteandtommy.com', 'CharlotteAndTommy.com');
+//         $m->to('tommyjmarshall@gmail.com', 'Tommy Marshall');
+//         $m->cc('cpince88@gmail.com', 'Charlotte Pince');
+//         $m->subject($answers->get('name') . ' has RSVPed!');
+//     });
 
-    return redirect('success');
-});
+//     return redirect('success');
+// });
